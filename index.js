b14d0990ce2ed023270756eaae8932f7d2d8aa12
Aaryan2002 <f20190713@pilani.bits-pilani.ac.in>
2021-08-05 15:36:49 +0530

    create the directory structure and setup the basics

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.use('/',require("./routes/index"));
+app.set('view engine','ejs');
+app.set('views','./views');
+app.listen(port,function(err){
+if(err){
+    console.log("Error:",err);
+}
+console.log("Server is up and running at",port);
+});
