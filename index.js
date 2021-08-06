4b9723625807b7722e90f341dd303fc4f27d4c5a
Aaryan2002 <f20190713@pilani.bits-pilani.ac.in>
2021-08-06 14:00:57 +0530

    create the template and required assets

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8000;
 
 app.use('/',require("./routes/index"));
+app.use(express.static('assets'));
 app.set('view engine','ejs');
 app.set('views','./views');
 app.listen(port,function(err){
